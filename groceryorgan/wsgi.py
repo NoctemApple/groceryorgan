8ab5a2231a1bb53bf8dba7d7080d45f4c335af77
NoctemApple <joshuaryanwong@gmail.com>
2025-03-27 15:33:55 +0800

    🚑 please work

--- groceryorgan/wsgi.py
+++ groceryorgan/wsgi.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from vercel_wsgi import VercelWSGIHandler
+from vercel_wsgi import VercelWSGIHandler  # Make sure this file is at the root
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'groceryorgan.settings')
 
 application = get_wsgi_application()
 
-# Expose the handler variable for Vercel
-handler = VercelWSGIHandler(application)
+# Wrap the Django WSGI app with our adapter.
+# Expose the callable under both 'app' and 'handler' to satisfy Vercel.
+app = VercelWSGIHandler(application)
+handler = app
